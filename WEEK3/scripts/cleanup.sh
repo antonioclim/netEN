ae81ddc1fb2ecbe7e712b43cbbac47809c8d097b
Revolvix <48681843+antonioclim@users.noreply.github.com>
2026-01-04 20:52:02 +0200

    Delete WEEK3 directory

--- WEEK3/scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "[CLEAN] Cleanup Mininet..."
-sudo mn -c 2>/dev/null || true
-echo "[CLEAN] Stopping procese Python..."
-sudo pkill -f "ex0[1-8]" 2>/dev/null || true
-sudo pkill -f "topo_" 2>/dev/null || true
-echo "[CLEAN] Cleanup temporary files..."
-rm -rf __pycache__ python/__pycache__ logs/ *.pcap *.pcapng
-echo "[OK] Cleanup complete."

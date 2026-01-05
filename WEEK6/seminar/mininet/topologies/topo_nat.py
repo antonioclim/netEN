206cb79f8c86779b308d20bce13154bcd8653618
Revolvix <48681843+antonioclim@users.noreply.github.com>
2026-01-05 14:06:58 +0200

    Delete WEEK6 directory

--- WEEK6/seminar/mininet/topologies/topo_nat.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys
-from topo_nat import main
-if __name__ == '__main__':
-    sys.exit(main())
--- WEEK6/seminar/mininet/topologies/topo_sdn.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys
-from topo_sdn import main
-if __name__ == '__main__':
-    sys.exit(main())
